09dced3a223a0f070ce2663788cdd6f5ccd2bf80
dragon15098 <dragon15098@gmail.com>
2017-02-24 08:19:11 +0700

    Update 20.8.1.py

--- 20.8.1.py
+++ 20.8.1.py
@@ -5,6 +5,6 @@ for i in range(len(a)):
        d[a[i]] = 1
     else:
         d[a[i]] = d[a[i]] + 1
-for key, value in d.items():
+for key, value in sorted(d.items()):
        if(value > 0):
            print(key + "\t" , value)
